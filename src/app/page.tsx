9336f1a31a2b8daded19cd3d550ad14b65ab9f04
efe osah <onanefeosah@icloud.com>
2023-08-08 10:31:34 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { Box } from "@mui/material";
+
+export default function Home() {
+    return <Box>The Okes</Box>;
+}
